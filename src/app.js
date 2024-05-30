08c4d458d1af1963474b7688b93947953b7e76e7
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-05-30 18:43:00 +0000

    prettier

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let domain = "";
+
+  for (let i = 0; i < pronoun.length; i++) return pronoun;
+
+  console.log(document.querySelector("#dominio").innerHTML);
 };
