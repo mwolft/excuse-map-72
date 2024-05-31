29d68c396e21bb3beb3a67674444002263ce5f14
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-05-31 07:00:26 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -9,10 +9,19 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let dot = [".com", ".es"];
+  let domain = '';
 
-  let domain = "";
-
-  for (let i = 0; i < pronoun.length; i++) return pronoun;
-
-  console.log(document.querySelector("#dominio").innerHTML);
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let y = 0; y < dot.length; y++) {
+          domain += pronoun[i] + adj[j] + noun[k] + dot[y] + '<br>';
+        }
+      }
+    }
+  }
+  
+  console.log(domain);
+  document.querySelector("#dominio").innerHTML = domain;
 };
