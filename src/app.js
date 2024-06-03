1692f6a9bb3d727311885a4dffeec2b4eef96ec8
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-06-03 06:01:24 +0000

    code-audit

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
 
 window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
   let dot = [".com", ".es"];
-  let domain = '';
 
+  let domain = '';
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let y = 0; y < dot.length; y++) {
-          domain += pronoun[i] + adj[j] + noun[k] + dot[y] + '<br>';
+          domain += '<p>' + pronoun[i] + adj[j] + noun[k] + dot[y] + '</p>';
         }
       }
     }
